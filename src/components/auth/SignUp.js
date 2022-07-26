dccf1501db756fd0b64169fc2616bc1a94c1cc63
sanchadr002 <sanchadr002@gmail.com>
2022-07-25 20:37:41 -0700

    set up sign up component

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,114 @@
+// import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { signUp, signIn } from '../../api/auth'
+import messages from '../shared/AutoDismissAlert/messages'
+
+import Form from 'react-bootstrap/Form'
+import {Container, Button} from 'react-bootstrap'
+
+const SignUp = (props) => {
+	// constructor(props) {
+	// 	super(props)
+
+	// 	this.state = {
+	// 		email: '',
+	// 		password: '',
+	// 		passwordConfirmation: '',
+	// 	}
+	// }    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+    const navigate = useNavigate()
+
+	const onSignUp = (event) => {
+		event.preventDefault()
+
+		const { msgAlert, setUser } = props
+
+        const credentials = {email, password, passwordConfirmation}
+
+		signUp(credentials)
+			.then(() => signIn(credentials))
+			.then((res) => setUser(res.data.user))
+			.then(() =>
+				msgAlert({
+					heading: 'Sign Up Success',
+					message: messages.signUpSuccess,
+					variant: 'success',
+				})
+			)
+			.then(() => navigate('/'))
+			.catch((error) => {
+                setEmail('')
+                setPassword('')
+                setPasswordConfirmation('')
+				msgAlert({
+					heading: 'Sign Up Failed with error: ' + error.message,
+					message: messages.signUpFailure,
+					variant: 'danger',
+				})
+			})
+	}
+
+
+    return (
+        <Container
+        style={{ 
+            marginTop: "10%",
+            padding: "10px",
+            width: '50%', 
+            boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+            fontFamily: "Times New Roman" }} >
+        <div className='row'>
+            <div className='col-sm-10 col-md-8 mx-auto mt-5'>
+                <h3>Sign Up</h3>
+                <Form onSubmit={onSignUp}>
+                    <Form.Group controlId='email'>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            required
+                            type='email'
+                            name='email'
+                            value={email}
+                            placeholder='Enter email'
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            required
+                            name='password'
+                            value={password}
+                            type='password'
+                            placeholder='Password'
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId='passwordConfirmation'>
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control
+                            required
+                            name='passwordConfirmation'
+                            value={passwordConfirmation}
+                            type='password'
+                            placeholder='Confirm Password'
+                            onChange={e => setPasswordConfirmation(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button style ={{marginTop: "2%"}} variant='primary' type='submit'>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    </Container>
+    )
+
+}
+
+export default SignUp
