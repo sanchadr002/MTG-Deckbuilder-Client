993b2169b0da621a5dbd57ef54f87f5150d2d647
sanchadr002 <sanchadr002@gmail.com>
2022-07-27 17:51:40 -0700

    import necessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // import dependencies
+// authorization and navigation dependencies
 import React, { useState, Fragment } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { v4 as uuid } from 'uuid'
 import AutoDismissAlert from './components'
 import RequireAuth from './components/shared/RequireAuth'
+import SignUp from './components/auth/SignUp'
+import SignIn from './components/auth/SignIn'
+import SignOut from './components/auth/SignOut'
+import ChangePassword from './components/auth/ChangePassword'
 
 function App() {
   return (
