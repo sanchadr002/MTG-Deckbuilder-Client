67941aef6cc59dd6c20192e8de97f9a98faef474
sanchadr002 <sanchadr002@gmail.com>
2022-07-27 17:25:14 -0700

    set up Home page skeleton

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+// import dependencies
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// Home component
+// Home will act as a landing page
+// Home will contain the site's instructions and suggestions for use
+    // MTG is a complicated game!
+// User will navigate to the various components of the application from here
+const Home = (props) => {
+    console.log ('props in home', props)
+
+    return (
+        <>
+            {/* Add instructions for application use */}
+            {/* Add navigation links to different functionalities of the application */}
+        </>
+    )
+}
