57d88bdb1d040887387e3e2f3471c90dd48238e0
sanchadr002 <sanchadr002@gmail.com>
2022-07-28 19:42:03 -0700

    add button to home page that calls for random card from scryfall and logs it to console

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 // import dependencies
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 // Home component
 // Home will act as a landing page
@@ -9,11 +10,22 @@ import { Link } from 'react-router-dom'
 // User will navigate to the various components of the application from here
 const Home = (props) => {
 
+    const randomCard = () => {
+        axios('https://api.scryfall.com/cards/random')
+            .then(card => console.log(card))
+            .catch(() => console.error())
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        randomCard()
+    }
+
     return (
         <>
-            <div>
-                <p> hi there</p>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="submit" value="random card"/>
+            </form>
         </>
     )
 }
