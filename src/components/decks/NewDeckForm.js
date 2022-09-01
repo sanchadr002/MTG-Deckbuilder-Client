e63a64c1df1160059a4bf91f613763a7eec9b2d8
sanchadr002 <sanchadr002@gmail.com>
2022-09-01 15:50:46 -0700

    new deck form navigates to card search form on submit

--- src/components/decks/NewDeckForm.js
+++ src/components/decks/NewDeckForm.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const NewDeckForm = (props) => {
+
+    const navigate = useNavigate()
+
     // Add onSubmit function to create an empty deck that the user will update with each card addition
     // function should also navigate user to the card search form
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        navigate('/cards/search')
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Label>Deck name</Form.Label>
             <Form.Control type="text" placeholder="Enter deck name"/>
             <Button type="submit">
